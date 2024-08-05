6509c5ad7f9a0dcbdef54b89eca7a2d32ac11b10
Armando Ruocco <armando.ruocco.dev@gmail.com>
2024-08-05 17:02:45 +0200

    feat: add `GetKind` helper (#46)
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- pkg/pluginhelper/helper.go
+++ pkg/pluginhelper/helper.go
@@ -181,3 +181,16 @@ func (*Data) DecodeBackup(backupDefinition []byte) (*apiv1.Backup, error) {
 
 	return &backup, nil
 }
+
+// GetKind gets the Kubernetes object kind from its JSON representation
+func GetKind(definition []byte) (string, error) {
+	var genericObject struct {
+		Kind string `json:"kind"`
+	}
+
+	if err := json.Unmarshal(definition, &genericObject); err != nil {
+		return "", err
+	}
+
+	return genericObject.Kind, nil
+}
