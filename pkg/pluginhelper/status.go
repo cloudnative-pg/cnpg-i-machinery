8eb93b533327856e2bc94abcd24c92974ddf27f8
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2024-08-07 13:22:12 +0200

    chore: review
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

--- pkg/pluginhelper/status.go
+++ pkg/pluginhelper/status.go
@@ -8,40 +8,63 @@ import (
 	"github.com/cloudnative-pg/cnpg-i/pkg/operator"
 )
 
-// SetStatusResponseBuilder a SetStatus response builder
+// ErrNilObject is used when a nill object is passed to the builder.
+var ErrNilObject = errors.New("nil object passed, use NoOpResponse")
+
+// NotAnObjectError is used when the passed value cannot be represented
+// as a JSON object.
+type NotAnObjectError struct {
+	representation []byte
+}
+
+func (err NotAnObjectError) Error() string {
+	return fmt.Sprintf(
+		"the passed variable cannot be serialized as a JSON object: %s",
+		err.representation,
+	)
+}
+
+// SetStatusResponseBuilder a SetStatus response builder.
 type SetStatusResponseBuilder struct{}
 
-// NewSetStatusResponseBuilder is an helper that creates the SetStatus endpoint responses
+// NewSetStatusResponseBuilder is an helper that creates the SetStatus endpoint responses.
 func NewSetStatusResponseBuilder() *SetStatusResponseBuilder {
 	return &SetStatusResponseBuilder{}
 }
 
-// NoOpResponse this response will ensure that no changes will be done to the plugin status
+// NoOpResponse this response will ensure that no changes will be done to the plugin status.
 func (s SetStatusResponseBuilder) NoOpResponse() *operator.SetClusterStatusResponse {
 	return &operator.SetClusterStatusResponse{JsonStatus: nil}
 }
 
-// SetEmptyStatusResponse will set the plugin status to an empty object '{}'
+// SetEmptyStatusResponse will set the plugin status to an empty object '{}'.
 func (s SetStatusResponseBuilder) SetEmptyStatusResponse() *operator.SetClusterStatusResponse {
-	b, _ := json.Marshal(map[string]string{})
+	b, err := json.Marshal(map[string]string{})
+	if err != nil {
+		panic("JSON mashaller failed for empty map")
+	}
+
 	return &operator.SetClusterStatusResponse{JsonStatus: b}
 }
 
-// JsonStatusResponse requires a struct or map that can be translated to a JSON object, will set the status to the passed
-// object
-func (s SetStatusResponseBuilder) JsonStatusResponse(obj any) (*operator.SetClusterStatusResponse, error) {
+// JSONStatusResponse requires a struct or map that can be translated to a JSON object,
+// will set the status to the passed object.
+func (s SetStatusResponseBuilder) JSONStatusResponse(obj any) (*operator.SetClusterStatusResponse, error) {
 	if obj == nil {
-		return nil, errors.New("nil object passed, use NoOpResponse")
+		return nil, ErrNilObject
 	}
-	b, err := json.Marshal(obj)
+
+	jsonObject, err := json.Marshal(obj)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("while marshalling resource definition: %w", err)
 	}
+
 	var js map[string]interface{}
-	if err := json.Unmarshal(b, &js); err != nil {
-		return nil, fmt.Errorf("invalid json: not an object: '%s'", string(b))
+	if err := json.Unmarshal(jsonObject, &js); err != nil {
+		return nil, NotAnObjectError{representation: jsonObject}
 	}
+
 	return &operator.SetClusterStatusResponse{
-		JsonStatus: b,
+		JsonStatus: jsonObject,
 	}, nil
 }
